fafa3d201623342f7d12f9997a08526760f5a929
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-11-08 01:01:53 -0700

    Reorganized code & added documentation.

--- SnapRoulette/src/com/example/snaproulette/App.java
+++ SnapRoulette/src/com/example/snaproulette/App.java
@@ -1,21 +1,24 @@
 package com.example.snaproulette;
+
 import com.parse.Parse;
 import com.parse.ParseObject;
 
 import android.app.Application;
 
-
 public class App extends Application {
 
+	@Override
 	public void onCreate(){
 		super.onCreate();
-		Parse.enableLocalDatastore(this);
-		System.out.println("before");
-	Parse.initialize(this, "KaobAtZ2RN71vQlWoZWPKVD51cqCIPirGfTsSq8m",
-    		"AuyBC5IMWQttOIhhoQ44mxEMpQ0IelFsDyX8gFTO");
-    ParseObject testObject = new ParseObject("TestObject");
-    testObject.put("redbull","dont give me wings");
-    testObject.saveInBackground();
-    System.out.println("finished");
+
+		// Configure the applicaiton with the Parse SDK
+		Parse.initialize(this, "KaobAtZ2RN71vQlWoZWPKVD51cqCIPirGfTsSq8m",
+			"AuyBC5IMWQttOIhhoQ44mxEMpQ0IelFsDyX8gFTO");
+
+		// Test the connection to Parse by creating a new object and 
+		// saving it to the server.
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("redbull","dont give me wings");
+		testObject.saveInBackground();
 	}
 }

65b8a2dd4852b736114124a766f6c3e4dddc2aad
brandonpon96 <brandonpon96@gmail.com>
2014-11-07 23:23:58 -0800

    Parse integration + id stuff

--- SnapRoulette/src/com/example/snaproulette/MainActivity.java
+++ SnapRoulette/src/com/example/snaproulette/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.snaproulette;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.parse.Parse;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.initialize(this, "13Zp2eWvtRH4inAQYcT274S7M5NvsAWFUcSb9YAw"
+        		, "ScLzSvB6bnCoPv9dDdoXV2jWM6aPXCx7FSdOzAy9");
     }
 
 

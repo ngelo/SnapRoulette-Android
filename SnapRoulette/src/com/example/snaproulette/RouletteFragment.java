baa32a4b664f437690db9b43b4475614e2be910b
Nikolas Gelo <nikolas.gelo@gmail.com>
2014-11-08 16:15:16 -0800

    Added todo & changed query to only get 1

--- SnapRoulette/src/com/example/snaproulette/RouletteFragment.java
+++ SnapRoulette/src/com/example/snaproulette/RouletteFragment.java
@@ -8,31 +8,37 @@ import android.graphics.BitmapFactory;
 import android.widget.ImageView;
 
 public class RouletteFragment extends Fragment {
-	
+
 	ParseFile file;
 
 	public void getSnap() {
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Snap");
 
-		query.getInBackground("xWMyZ4YEGZ", new GetCallback<ParseObject>() {			
-		  public void done(ParseObject object, ParseException e) {
-		    if (e == null) { 
-		    	file = object.getParseFile("imageFile");
-		    	// return the string name for now, display the byte[] when finished
-		    	System.out.println(file.getName());
-		    } else {
-		    	System.out.println("Oh Shit");
-		      // something went wrong
-		    }
-	    	
-		  }
+		// TODO Add filters
+		// 1.) Where hasBeenViewed = false
+		// 2.) Where the user id of the snap is not equal to the current user
+		// 3.) Get the oldest one (or randomly)
+
+		query.getFirstInBackground(new GetCallback<ParseObject>() {
+			public void done(ParseObject object, ParseException e) {
+				if (e == null) {
+					file = object.getParseFile("imageFile");
+					// return the string name for now, display the byte[] when
+					// finished
+					System.out.println(file.getName());
+				} else {
+					System.out.println("Oh Shit");
+					// something went wrong
+				}
+
+			}
 
 		});
 		try {
 			byte[] bytes = file.getData();
 			Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
 			// need to implement past null for imageView
-			ImageView image= new ImageView(null);
+			ImageView image = new ImageView(null);
 			image.setImageBitmap(bmp);
 		} catch (ParseException e1) {
 			// TODO Auto-generated catch block
